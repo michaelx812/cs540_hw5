7a4f071512e5dfae08e72da2e90ff9d75b156c6f
Ge Xu <michaelx@GematoMacBook-Pro.local>
2019-11-29 20:45:42 -0600

    test

--- NaiveBayesClassifier.java
+++ NaiveBayesClassifier.java
@@ -2,7 +2,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import jdk.jfr.Label;
+import Label;
 
 /**
  * Your implementation of a naive bayes classifier. Please implement all four methods.
